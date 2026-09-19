0662e0fc8a7017a6cec59d3149c33b26230dcb86
soydoyin <prototydxd24@gmail.com>
2026-09-18 19:20:34 -0500

    primer commit

---
+++ src/test/java/com/example/estudiantes/EstudiantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estudiantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudiantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
